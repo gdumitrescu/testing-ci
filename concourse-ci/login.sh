f9c05984a5a9f33461e590d7b548b764f98998eb
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2018-05-03 22:56:10 -0400

    Update concourse-ci login.sh

--- concourse-ci/login.sh
+++ concourse-ci/login.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-USERNAME=$(lpass show 6437354096189029998 --username)
-PASSWORD=$(lpass show 6437354096189029998 --password)
-URL=$(lpass show 6437354096189029998 --url)
-CA_CERT=$(lpass show 6437354096189029998 --notes)
+USERNAME=$(lpass show 7906497051561120514 --username)
+PASSWORD=$(lpass show 7906497051561120514 --password)
+URL=$(lpass show 7906497051561120514 --url)
 
 TARGET=$1
 TEAM=$2
 
 if [ -z "$TARGET" ]; then
-  TARGET=airplane
+  TARGET=cibox
 fi
 
 if [ -z "$TEAM" ]; then
@@ -31,8 +30,7 @@ function login() {
     --concourse-url="$URL" \
     --username="$USERNAME" \
     --password="$PASSWORD" \
-    --team-name="$TEAM" \
-    --ca-cert=caCert.pem
+    --team-name="$TEAM"
 }
 
 function logout() {
@@ -44,10 +42,8 @@ function sync() {
 }
 
 function main() {
-  creds
   login
   sync
-  clean
 }
 
 main
